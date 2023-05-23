6e622989e6bd16abed633627db82c5e211bd2b26
hbcheng7 <133923635+hbcheng7@users.noreply.github.com>
2023-05-23 10:31:55 -0700

    Update PeakFinder.py

--- PeakFinder.py
+++ PeakFinder.py
@@ -1,1 +1,33 @@
+#WE WANT TO FIND PEAKS
+#PEAKS ARE AREAS WHERE COVERAGE IN SEQUENCING DATA IS MUCH HIGHER, HIGHER READ COUNTS
+#we need to approach this as a interval scheduling problem ,keeping track of number of reads per base pair
+#we need a metric to define how many counts and how an increasing count trend can act as a peak.
 
+
+#OUTLINE:
+
+#Function to read TSV Files from each chromosome, and select the chromosome of either user choice or by default select one with most data
+def processTagTSVs():
+  #process a dictionary list of start end intervals, of pair each. Key = start coordinate, value = (START, END)
+  intervalList = {0:(0,1)}
+  #for loop given amount of intervals to add to the list
+  return intervalList
+
+#Function to count number of reads per BP. 
+def countReads(intervalList):
+  #keep track of each BP and its read count in a dictionary. Key = int bp, Value = Count of Reads
+  allCounts = {}
+  #go through each interval and count up number of reads per BP. time compleixty might suck but start with this
+  
+  
+ return
+#function to process trends in data and idefntiy peaks given thresholds of significance
+def findPeaks():
+  #return a list of start end values of peaks
+  return peaks
+
+#function to process output BED file, write using python Output stream
+def createBED():
+  return BED
+
+  
